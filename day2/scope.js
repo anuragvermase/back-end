2e699837f74b8a98a4bb9ba3486229a0af63f41e
Anurag Verma <anuragver689@gmail.com>
2024-02-11 22:52:47 +0530

    Done

---
+++ day2/scope.js
@@ -0,0 +1,61 @@
+//Scope
+
+/**Three type of scope:
+ * global scope
+ * function scope
+ * block scope
+ */
+
+// Global Scope
+ 
+let name = "Great"   //Global Scope
+
+console.log(name);  //accessible outside function also
+
+function printName(){
+    console.log(name); //accessible in function 
+}
+printName()
+
+
+//Local scope /function scope variable
+
+                                                                
+function num(){
+    let one = 1;
+    console.log(one);
+
+} 
+num()
+
+// console.log(one); //shows error (not accessible) 
+
+
+//Block scope
+
+
+/**
+ * {
+ *    ---> scope
+ * }
+ * 
+ * 
+ *  
+{
+    var x=20            --->  var is not a block scope
+    console.log(x);  //20
+}
+
+console.log(x);  //20
+
+*/
+
+
+{
+    let y = 20;       // ---> block scope
+    console.log(y); // 20
+}
+
+console.log(y);   // Error (ReferenceError: y is not defined)
+
+
