850aa0a698e5f17be0b3e6e8f88eda5c387eac93
Anurag Verma <anuragver689@gmail.com>
2024-02-23 22:01:01 +0530

    express

---
+++ day9_Express/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+console.log(typeof express);
+
+const app = express()
+
+//starting a express server
+
+app.listen(8080, ()=>{
+    console.log("hello great! server got started");
+})
